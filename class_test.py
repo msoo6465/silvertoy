54f5d7d1fa208d05ad47d933423cabf01887cdac
mskim_raspi <msoo6465@gmail.com>
2021-10-20 19:28:30 +0900

    test script

--- class_test.py
+++ class_test.py
@@ -1,4 +1,15 @@
 from move import move_function
-
-mv = move_function(0)
+import time
+from datetime import datetime
+print('follow start')
+mv = move_function(1)
 mv.start()
+print(datetime.now())
+while True:
+    time.sleep(10)
+    print('moving')
+    if mv.get_is_end() == 1:
+        print(datetime.now())
+        print('end moving')
+        break
+print('end')
